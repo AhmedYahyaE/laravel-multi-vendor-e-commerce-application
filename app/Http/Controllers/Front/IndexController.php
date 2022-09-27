84b0c5b9e46aac7417731cd27296a20374bf6a33
Ahmed Yahya <a.yahya1@yahoo.com>
2022-09-27 21:09:18 +0200

    Finished 'Featured Products'

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -18,16 +18,21 @@ class IndexController extends Controller
         $newProducts   = \App\Models\Product::orderBy('id', 'Desc')->where('status', 1)->limit(8)->get()->toArray(); // show the LATEST (DESCendingly) 8 added products (to show the 'New Arrivals' at the home page)    // Ordering, Grouping, Limit & Offset: https://laravel.com/docs/9.x/queries#ordering-grouping-limit-and-offset    // https://www.youtube.com/watch?v=T_CWdKW5he0&list=PLLUtELdNs2ZaAC30yEEtR6n-EPXQFmiVu&index=71
         $bestSellers   = \App\Models\Product::where([
             'is_bestseller' => 'Yes',
-            'status' => 1 // product is enabled (active)
+            'status'        => 1 // product is enabled (active)
         ])->inRandomOrder()->get()->toArray(); // show the 'BestSeller' products with RANDOM ORDERING: https://laravel.com/docs/9.x/queries#random-ordering    // using the inRandomOder() method    // Only 'superadmin' can mark a product as 'best seller', but 'vendor' can't    // https://www.youtube.com/watch?v=jCWz6fqoFno&list=PLLUtELdNs2ZaAC30yEEtR6n-EPXQFmiVu&index=72
         $discountedProducts = \App\Models\Product::where('product_discount', '>' , 0)->where('status', 1)->limit(6)->inRandomOrder()->get()->toArray(); // show 'Discounted Products' with RANDOM ORDERING    // https://www.youtube.com/watch?v=sCTv1gw201g&list=PLLUtELdNs2ZaAC30yEEtR6n-EPXQFmiVu&index=74
+        $featuredProducts   = \App\Models\Product::where([
+            'is_featured' => 'Yes',
+            'status'      => 1 // product is enabled (active)
+        ])->limit(6)->get()->toArray(); // show 'Featured Products'    // https://www.youtube.com/watch?v=4cMus91lkDs&list=PLLUtELdNs2ZaAC30yEEtR6n-EPXQFmiVu&index=74
         // dd($fixBanners);
         // dd($newProducts);
         // dd($bestSellers);
         // dd($discountedProducts);
+        // dd($featuredProducts);
 
 
         // return view('front.index')->with(compact('banners')); // this is the same as:    return view('front/index');
-        return view('front.index')->with(compact('sliderBanners', 'fixBanners', 'newProducts', 'bestSellers', 'discountedProducts')); // this is the same as:    return view('front/index');
+        return view('front.index')->with(compact('sliderBanners', 'fixBanners', 'newProducts', 'bestSellers', 'discountedProducts', 'featuredProducts')); // this is the same as:    return view('front/index');
     }
 }
