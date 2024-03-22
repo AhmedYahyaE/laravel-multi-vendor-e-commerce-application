9f6d8c6ee849f2f797cc1a9d712f9992bd708376
Niño Feliciano <ninofeliciano9@gmail.com>
2024-03-22 10:21:36 +0800

    prev button collection pagination

--- resources/views/front/products/collection_listings.blade.php
+++ resources/views/front/products/collection_listings.blade.php
@@ -326,13 +326,31 @@
             data-settings="{&quot;content_width&quot;:&quot;full&quot;,&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;container_type&quot;:&quot;flex&quot;}"
         >
             {{-- link to prev page --}}
+
+            <div class="elementor-element elementor-element-831ada5 elementor-align-left elementor-widget elementor-widget-button" data-id="831ada5" data-element_type="widget" data-widget_type="button.default">
+				<div class="elementor-widget-container">
+                    <div class="elementor-button-wrapper">
+                        <a class="elementor-button elementor-button-link elementor-size-sm" href="#">
+                            <span class="elementor-button-content-wrapper">
+                                <span class="elementor-button-icon elementor-align-icon-left">
+                                    <svg aria-hidden="true" class="e-font-icon-svg e-fas-angle-left" viewBox="0 0 256 512" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path>
+                                    </svg>			
+                                </span>
+                                <span class="elementor-button-text"></span>
+                            </span>
+                        </a>
+                    </div>
+				</div>
+            </div>
+           
+
             @if ($collection->currentPage() > 1)
             <div
                 class="elementor-element elementor-element-4e10030 elementor-align-left elementor-widget elementor-widget-button"
                 data-id="4e10030"
                 data-element_type="widget"
-                data-widget_type="button.default"
-            >
+                data-widget_type="button.default">
                 <div class="elementor-widget-container">
                     <div class="elementor-button-wrapper">
                         <a class="elementor-button elementor-button-link elementor-size-sm" href="{{$collection->previousPageUrl()}}">
@@ -449,8 +467,7 @@
                 class="elementor-element elementor-element-4e10030 elementor-align-left elementor-widget elementor-widget-button"
                 data-id="4e10030"
                 data-element_type="widget"
-                data-widget_type="button.default"
-            >
+                data-widget_type="button.default">
                 <div class="elementor-widget-container">
                     <div class="elementor-button-wrapper">
                         <a class="elementor-button elementor-button-link elementor-size-sm" href="{{$collection->nextPageUrl()}}">
