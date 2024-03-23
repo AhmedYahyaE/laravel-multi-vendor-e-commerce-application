03e753dc8e435ec0c287e2cdfc413fa9ec58f3a9
IK Mendoza <iken.mndz@gmail.com>
2024-03-23 21:04:53 +0800

    [Vendor] Update Password

--- resources/views/admin/settings/update_admin_password.blade.php
+++ resources/views/admin/settings/update_admin_password.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-md-12 grid-margin">
                     <div class="row">
                         <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                            <h3 class="font-weight-bold">Admin Settings</h3>
+                            <h3 class="font-weight-bold">User Management</h3>
                         </div>
                         <div class="col-12 col-xl-4">
                             <div class="justify-content-end d-flex">
@@ -61,7 +61,7 @@
 
 
 
-                            <h4 class="card-title">Update Admin Password</h4>
+                            <h4 class="card-title">Update Password</h4>
 
 
 
@@ -70,11 +70,12 @@
 
 
                                 <div class="form-group">
-                                    <label>Admin Username/Email</label>
-                                    <input class="form-control" value="{{ $adminDetails['email'] }}" readonly> <!-- Check updateAdminPassword() method in AdminController.php -->
+                                    <label>Username/Email</label>
+                                    <input class="form-control" value="{{ $adminDetails['email'] }}" readonly> 
+                                    <!-- Check updateAdminPassword() method in AdminController.php -->
                                 </div>
                                 <div class="form-group">
-                                    <label>Admin Type</label>
+                                    <label>Type</label>
                                     <input class="form-control" value="{{ $adminDetails['type'] }}" readonly>
                                 </div>
                                 <div class="form-group">
