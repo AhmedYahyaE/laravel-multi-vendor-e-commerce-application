ab559f9c72d54ae7ab639d7ad1894dcfc9b1daf9
IK Mendoza <iken.mndz@gmail.com>
2024-03-23 22:36:05 +0800

    [Customer] Review only when ordered

--- app/Models/OrdersProduct.php
+++ app/Models/OrdersProduct.php
@@ -9,7 +9,13 @@ class OrdersProduct extends Model
 {
     use HasFactory;
 
+    protected $table = 'orders_products';
+
     public function product_category() {
         return $this->hasOneThrough('\App\Models\Category', '\App\Models\Product', 'id', 'id');
+    }
+
+    public static function hasUserOrderedThisProduct($user_id, $product_id) {
+        return OrdersProduct::select('id')->where('user_id', $user_id)->where('product_id', $product_id)->get()->count() > 0 ? true:false;
     }
 }
